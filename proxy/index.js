2cb2bad1ab445e45ac3209a0d6e4763f57f050a0
Alissa Colascione <acolascione27@gmail.com>
2020-10-07 00:22:24 -0400

    fixed sticky reservations

--- proxy/index.js
+++ proxy/index.js
@@ -3,6 +3,10 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 const PORT = 3005;
+const cors = require('cors');
+
+app.use(cors());
+
 
 app.use('/:id', express.static(path.join(__dirname, './public')));
 
