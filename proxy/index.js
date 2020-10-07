14f920c212777dd422141bd7fbd38abe2c506151
Alissa Colascione <acolascione27@gmail.com>
2020-10-07 18:34:33 -0400

    fixed to render without cors

--- proxy/index.js
+++ proxy/index.js
@@ -4,37 +4,38 @@ const path = require('path');
 const app = express();
 const PORT = 3005;
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use('/:id', express.static(path.join(__dirname, './public')));
 
-
-
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//   app.use(
-//     '/:id',
-//     createProxyMiddleware({
-//       target: 'http://localhost:3003',
-//       changeOrigin: true,
-//     })
-//   );
-//   app.use(
-//     '/:id',
-//     createProxyMiddleware({
-//       target: 'http://localhost:3001',
-//       changeOrigin: true,
-//     })
-//   );
-//   app.use(
-//     '/:id',
-//     createProxyMiddleware({
-//       target: 'http://localhost:3002',
-//       changeOrigin: true,
-//     })
-//   );
-// };
-
+  app.use(
+    '/listings/header/:id',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/listings/gallery/:id',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/listings/reviews/:id',
+    createProxyMiddleware({
+      target: 'http://localhost:3003',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/listings/moreplaces/:id',
+    createProxyMiddleware({
+      target: 'http://localhost:3002',
+      changeOrigin: true,
+    })
+  );
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
