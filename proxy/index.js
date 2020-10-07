e2d2a406ef60d03c4ede6c6577ce610deb682e57
Alissa Colascione <acolascione27@gmail.com>
2020-10-07 14:21:47 -0400

    serving bundle.js from aws s3

--- proxy/index.js
+++ proxy/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
+
 const app = express();
 const PORT = 3005;
-const cors = require('cors');
-
-app.use(cors());
 
 
 app.use('/:id', express.static(path.join(__dirname, './public')));
 
+
+
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//   app.use(
+//     '/:id',
+//     createProxyMiddleware({
+//       target: 'http://localhost:3003',
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     '/:id',
+//     createProxyMiddleware({
+//       target: 'http://localhost:3001',
+//       changeOrigin: true,
+//     })
+//   );
+//   app.use(
+//     '/:id',
+//     createProxyMiddleware({
+//       target: 'http://localhost:3002',
+//       changeOrigin: true,
+//     })
+//   );
+// };
 
 
 
